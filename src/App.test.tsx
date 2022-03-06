aa68b5546b3bd82f2d7a8acda98dcdc63dd2fbc7
Zafer AYAN <ozcanzaferayan@gmail.com>
2022-03-06 21:33:13 +0300

    Add jest-mock-fetch function

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+import fetchMock from "jest-fetch-mock";
+import fetch from "jest-fetch-mock";
 
-test('renders learn react link', () => {
+fetchMock.enableMocks();
+
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+test("renders user data", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const button = screen.getByText(/Get user/i);
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      name: "The Octocat",
+      followers: "100",
+      following: "200",
+    })
+  );
+
+  fireEvent.click(button);
+
+  const txtUsername = await screen.findByText(/The Octocat/i);
+  expect(txtUsername).toBeInTheDocument();
 });
